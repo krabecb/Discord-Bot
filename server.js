75cca4cc4853378b8f6aab024ae675a4cc2190a3
krabecb <60526718+krabecb@users.noreply.github.com>
2021-07-03 01:20:01 -0500

    Bot command test works

--- server.js
+++ server.js
@@ -7,12 +7,26 @@ const token = process.env.TOKEN
 const client = new Discord.Client()
 
 //Initiate a command with this prefix.
-const prefix = '-'
+const prefix = '-'	
 
 
 //'ready' is the event
 client.once('ready', () => {
 	console.log("Bot is online!")
+})
+
+client.on('message', message => {
+	//1. If message has a prefix, then continue. Otherwise, ignore message.
+	//2. Check if the bot initiated the command, stop process to avoid crashes.
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+	//slice lets you have multiple commands
+	const args = message.content.slice(prefix.length).split(/ +/) // / +/ is the same as ""
+	const command = args.shift().toLowerCase()
+
+	if(command === 'test') {
+		message.channel.send('Hello! I am succesfully running! :3')
+	}
 })
 
 //Log into Discord bot
