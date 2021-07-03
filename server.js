d0993c58ca131ef8dfb12f8bbd1e8614376d4c95
krabecb <60526718+krabecb@users.noreply.github.com>
2021-07-03 02:21:45 -0500

    Bot commands are now modular

--- server.js
+++ server.js
@@ -7,7 +7,19 @@ const token = process.env.TOKEN
 const client = new Discord.Client()
 
 //Initiate a command with this prefix.
-const prefix = '-'	
+const prefix = '-'
+
+const fs = require('fs')
+
+client.commands = new Discord.Collection()
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+//Loop through files
+for(const file of commandFiles) {
+	const command = require(`./commands/${file}`)
+
+	client.commands.set(command.name, command)
+}
 
 
 //'ready' is the event
@@ -24,8 +36,9 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).split(/ +/) // / +/ is the same as ""
 	const command = args.shift().toLowerCase()
 
+	//List as many commands with else if
 	if(command === 'test') {
-		message.channel.send('Hello! I am succesfully running! :3')
+		client.commands.get('test').execute(message, args)
 	}
 })
 
