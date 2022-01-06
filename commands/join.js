b09fec048b32d4c920d2d7a9994016a1b317b85c
krabecb <60526718+krabecb@users.noreply.github.com>
2022-01-06 17:05:51 -0600

    Music plays

--- commands/join.js
+++ commands/join.js
@@ -1,5 +1,8 @@
+const ytdl = require('ytdl-core')
+const ytSearch = require('yt-search')
+
 module.exports = {
-	name: 'join',
+	name: 'company',
 	description: 'Joins voice channel',
 	async execute(client, message, args) {
 		const voiceChannel = message.member.voice.channel
@@ -8,8 +11,27 @@ module.exports = {
 		const permissions = voiceChannel.permissionsFor(message.client.user)
 		if(!permissions.has('CONNECT')) return message.channel.send("You don't have the correct permissions")
 		if(!permissions.has('SPEAK')) return message.channel.send("You don't have the correct permissions")
-		// if(!args.length) return message.channel.send('You need to send the second argument') //<--Might just be for YT
+		// if(!args.length) return message.channel.send('You need to send the second argument') //Specific to a user querying YT
 
 		const connection = await voiceChannel.join()
+
+		const videoFinder = async (query) => {
+			const videoResult = await ytSearch(query)
+			return (videoResult.videos.length > 1) ? videoResult.videos[0] : null
+		}
+
+		const video = await videoFinder('lofi forest')
+
+		if(video) {
+			const stream = ytdl(video.url, {filter: 'audioonly'})
+			connection.play(stream, {seek: 0, volume: 1})
+			.on('finish', () => {
+				voiceChannel.leave()
+			})
+
+			await message.reply(`WEEU! Now playing: ${video.title }`)
+		} else {
+			message.channel.send('No video results found!')
+		}
 	}
 }
